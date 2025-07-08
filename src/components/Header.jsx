a163cf6a2b05c793aa8499c601c45bb5c5fb2958
Rafael Santos <rafaelsgoncalves04@gmail.com>
2025-07-08 13:18:44 -0300

    fix: remove the delete button from headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types"
 import { useState } from "react"
 
-import { AddIcon, TrashIcon } from "../assets/icons"
+import { AddIcon } from "../assets/icons"
 import AddTaskDialog from "./AddTaskDialog"
 import Button from "./Button"
 
@@ -17,11 +17,6 @@ function Header({ subtitle, title }) {
       </div>
 
       <div className="flex items-center gap-3">
-        <Button color="ghost">
-          Limpar tarefas
-          <TrashIcon />
-        </Button>
-
         <Button onClick={() => setAddTaskDialogIsOpen(true)}>
           <AddIcon />
           Nova tarefa
